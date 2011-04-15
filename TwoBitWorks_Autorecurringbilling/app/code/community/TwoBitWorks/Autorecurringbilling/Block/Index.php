17831600e257f871bcc1e6453705ab0b1de0b074
mike perez <meekprize@gmail.com>
2011-04-14 22:36:31 -0500

    trying to listen for events where our recurring billing logic may be called.

--- TwoBitWorks_Autorecurringbilling/app/code/community/TwoBitWorks/Autorecurringbilling/Block/Index.php
+++ TwoBitWorks_Autorecurringbilling/app/code/community/TwoBitWorks/Autorecurringbilling/Block/Index.php
@@ -1,8 +1,4 @@
-<?php   
-class TwoBitWorks_Autorecurringbilling_Block_Index extends Mage_Core_Block_Template{   
-
-
-
-
+<?php
+class TwoBitWorks_Autorecurringbilling_Block_Index extends Mage_Core_Block_Template {
 
 }
--- TwoBitWorks_Autorecurringbilling/app/code/community/TwoBitWorks/Autorecurringbilling/controllers/IndexController.php
+++ TwoBitWorks_Autorecurringbilling/app/code/community/TwoBitWorks/Autorecurringbilling/controllers/IndexController.php
@@ -1,9 +1,7 @@
 <?php
-class TwoBitWorks_Autorecurringbilling_IndexController extends Mage_Core_Controller_Front_Action{
-    public function IndexAction() {
-      
-	  $this->loadLayout();   
-      $this->renderLayout(); 
-	  
-    }
+class TwoBitWorks_Autorecurringbilling_IndexController extends Mage_Core_Controller_Front_Action {
+	public function indexAction() {
+		$this->loadLayout();
+		$this->renderLayout();
+	}
 }
